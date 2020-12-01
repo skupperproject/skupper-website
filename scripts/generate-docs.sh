7185f357eab6cd6ba4fa4aa5d10110f5009162df
Paul Wright <pwright@redhat.com>
2020-12-01 12:58:53 +0000

    make working locally

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,23 @@
+cd scripts
+
+# which directory should docs land in?
+docs_dir=docs2
+
+# clear temporary directory 
+rm -r antora_build/*
+
+# clear input directory
+
+rm -r ../input/$docs_dir/*
+
+# generate html in temporary directory (antora_build)
+antora --fetch docs-playbook.yml
+
+# copy html to input (removing skupper level)
+cp -r antora_build/skupper/* ../input/$docs_dir
+
+# rename all html files to *.in
+shopt -s globstar
+for file in ../input/$docs_dir/**/*.html; do
+  mv "$file" "${file%.abc}.in"
+done
