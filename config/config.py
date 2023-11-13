52168ce015bf365f7b1ef97e9e52316a3c38bfba
Paul Wright <pwright@redhat.com>
2023-11-13 18:33:36 +0000

    v1.5.0

--- config/config.py
+++ config/config.py
@@ -2,5 +2,5 @@ def path_nav(page):
     links = " <span class=\"path-separator\">&#8250;</span> ".join(list(page.path_nav_links)[1:])
     return f"<nav id=\"-path-nav\"><div>{links}</div></nav>"
 
-skupper_release = "1.4.3"
-skupper_release_date = "20 September 2023"
+skupper_release = "1.5.0"
+skupper_release_date = "10 November 2023"
