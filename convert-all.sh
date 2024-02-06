34e0ced46f1cd6774b59ee04eebb231a5ded42f0
Paul Wright <pwright@redhat.com>
2024-02-06 14:28:13 +0000

    add deploy concerns, fix in examples

--- convert-all.sh
+++ convert-all.sh
@@ -1,4 +1,20 @@
-./convert-adoc.sh cli/index.adoc cli/tokens.adoc cli/podman.adoc cli/native-security-options.adoc yaml/index.adoc operator/index.adoc console/index.adoc policy/index.adoc troubleshooting/index.adoc overview/connectivity.adoc overview/glossary.adoc overview/index.adoc overview/resources.adoc overview/routing.adoc overview/security.adoc
+./convert-adoc.sh \
+cli/index.adoc \
+cli/tokens.adoc \
+cli/podman.adoc \
+cli/native-security-options.adoc \
+yaml/index.adoc \
+operator/index.adoc \
+console/index.adoc \
+policy/index.adoc \
+troubleshooting/index.adoc \
+overview/connectivity.adoc \
+overview/glossary.adoc \
+overview/index.adoc \
+overview/resources.adoc \
+overview/routing.adoc \
+overview/security.adoc \
+kubernetes/deployment-concerns.adoc
 
 cp -r subrepos/skupper-docs/images/ input/docs/
 
