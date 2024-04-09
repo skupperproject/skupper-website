89fc0b961789fcfe4eb7fbfdbbefe9e54625e547
Paul Wright <pwright@redhat.com>
2024-04-09 16:05:49 +0100

    fix sed when converting adoc  to avoid multiple substitutions

--- convert-all.sh
+++ convert-all.sh
@@ -23,9 +23,9 @@ python python/nested-numbers.py input/docs/operator/
 
 # To workaround transform interpreting {{.ID}} as a var:
 
-sed -i 's/.ID/{.ID}/g' input/docs/cli/podman.md
-sed -i 's/.Image/{.Image}/g' input/docs/cli/podman.md
-sed -i 's/.Labels/{.Labels}/g' input/docs/cli/podman.md
+sed -i 's/.ID}} /{.ID}}} /g' input/docs/cli/podman.md
+sed -i 's/ {{.Image/ {{{.Image}/g' input/docs/cli/podman.md
+sed -i 's/ {{.Labels/ {{{.Labels}/g' input/docs/cli/podman.md
 
 # To workaround https://github.com/skupperproject/skupper-website/issues/81
 sed -i 's/   | /| /g' input/docs/kubernetes/deployment-concerns.md
